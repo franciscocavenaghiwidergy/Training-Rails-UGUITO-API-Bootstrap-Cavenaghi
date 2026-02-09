f2effcc96fcece9215d6020c05455fc07b9f2b1d
franciscocavenaghiwidergy <francisco.cavenaghi@widergy.com>
2026-02-09 08:58:15 -0300

    fix note spec

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -10,17 +10,15 @@ RSpec.describe Note, type: :model do
   it { is_expected.to validate_presence_of(:content) }
   it { is_expected.to belong_to(:user) }
 
-  RSpec.shared_examples 'review word limit validation' do |factory, limit|
-    let(:utility) { create(factory) }
-
-    it "is invalid when review content exceeds #{limit} words" do
+  RSpec.shared_examples 'review word limit validation' do
+    it 'is invalid when review content exceeds the word limit' do
       note.content = Faker::Lorem.words(number: limit + 1).join(' ')
 
       expect(note).not_to be_valid
       expect(note.errors[:content]).to be_present
     end
 
-    it "is valid when review content is exactly #{limit} words" do
+    it 'is valid when review content is exactly at the word limit' do
       note.content = Faker::Lorem.words(number: limit).join(' ')
 
       expect(note).to be_valid
@@ -28,11 +26,17 @@ RSpec.describe Note, type: :model do
   end
 
   context 'when user belongs to NorthUtility' do
-    it_behaves_like 'review word limit validation', :north_utility, 50
+    let(:utility) { create(:north_utility) }
+    let(:limit) { 50 }
+
+    it_behaves_like 'review word limit validation'
   end
 
   context 'when user belongs to SouthUtility' do
-    it_behaves_like 'review word limit validation', :south_utility, 60
+    let(:utility) { create(:south_utility) }
+    let(:limit) { 60 }
+
+    it_behaves_like 'review word limit validation'
   end
 
   context 'when note is not a review' do

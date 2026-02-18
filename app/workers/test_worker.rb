cf63f75d20b5580a5926cfc313bc94d43ab1af3d
franciscocavenaghiwidergy <francisco.cavenaghi@widergy.com>
2026-02-18 10:36:12 -0300

    add httparty

--- app/workers/test_worker.rb
+++ app/workers/test_worker.rb
@@ -1,7 +1,15 @@
 class TestWorker
   include Sidekiq::Worker
+
+  OPEN_LIBRARY_URL = 'https://openlibrary.org/api/books?bibkeys=ISBN:0385472579&format=json&jscmd=data'
+
+  def perform
+    execute
+  end
+
   def execute
-    sleep(30)
-    [200, { result: 'TEST WORKER' }]
+    response = HTTParty.get(OPEN_LIBRARY_URL)
+    parsed_body = response.body.present? ? JSON.parse(response.body) : nil
+    [response.code, parsed_body]
   end
 end
